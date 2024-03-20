c9bf6786034bbd1f74b04cbe7bdd032349ab5212
Fawazahmad75 <114078421+Fawazahmad75@users.noreply.github.com>
2024-03-20 03:09:02 -0230

    initial commmit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+     <p>home</p> 
+     {/*<UserButton afterSignOutUrl='/'/>*/}
+    </div>
+  )
+}
+
+export default home
